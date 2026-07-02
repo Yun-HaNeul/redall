09702eb8d2b6d8dba5ebf277ac3e25c8182ebc8a
Yun Ha-Neul <qnrty1243@neighbor21.co.kr>
2026-07-02 15:40:33 +0900

    first commit

---
+++ src/test/java/io/security/redall/RedallApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.redall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
